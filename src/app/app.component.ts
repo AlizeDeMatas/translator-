85b9928b3d61f9976d264de646141cc6892347b8
AlizeDeMatas <alize.dematas1@gmail.com>
2020-04-08 12:39:53 -0400

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { TranslateService } from "./translate.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'translator';
+  text: string;Yttik#1
+  
+
+  translatedText: string;
+
+  constructor(private translateService: TranslateService) {}
+  submit() {
+    this.translateService.translate(this.text).subscribe(result => {
+      this.translatedText = result;
+    });
+  }
 }
